d0c6ac746333417e45d7d9a30e83b462a36838c5
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-02-18 16:03:08 +0100

    Canvas set

---
+++ app.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module("FractalApp", []);
+
+app.service("FractalRenderer", function() {
+    var renderer = {
+        canvas: null,
+        render: function() {
+            if (this.canvas == null)
+                alert("Canvas not set");
+            else
+                alert("render");
+        },
+    };
+
+    return renderer;
+});
+
+app.directive("fractalCanvas", ["FractalRenderer", function (FractalRenderer) {
+    return {
+        restrict: "A",
+        transclude: true,
+        link: function (scope, element) {
+            FractalRenderer.canvas = element;
+        },
+    };
+}]);
+
+app.controller("FractalController", function($scope, FractalRenderer) {
+    $scope.renderFractal = function() {
+        FractalRenderer.render();
+    };
+});
